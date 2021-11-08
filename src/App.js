28f073584fd4c692d50c55b184b9ffdf27ca8863
Melody Nguyen <78733227+Melody1402@users.noreply.github.com>
2021-11-07 19:22:03 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import College from "./College"
+import User from "./User"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="shadow-css">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div style={{color:'black'}}>
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab Exercise</h2>
+        <User studentId="101281971" firstName="Thi Thuy" lastName="Nguyen"/>
+        <College name="George Brown College, Toronto"/>
+        </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
